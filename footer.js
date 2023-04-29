d0c694af720737297c38eb7f1fd875e1d8a53057
Emma Byron <112167467+emmabyron@users.noreply.github.com>
2023-04-28 21:33:58 -0400

    added pregithub work

---
+++ footer.js
@@ -0,0 +1,6 @@
+document.write(`
+<footer>
+    <p>jan Wika li pali e ni kulupu ilo.</p>
+    <p><i>Confused? This website is in the constructed language </i>toki pona<i>, designed as a minimalist language with about 100 words.</i> <a href="https://tokipona.org">more information</a></p>
+</footer>
+`);
---
+++ header.js
@@ -0,0 +1,11 @@
+document.write(`
+<header>
+    <h1>soweli.mu</h1>
+    <h3>kulupu ilo li jo e soweli mu pi pona mute</h3>
+    <nav>
+        <span class="navbar"><a href="index.html">tomo</a></span>
+        <span class="navbar">|</span>
+        <span class="navbar"><a href="contact.html">o toki</a></span>
+    </nav>
+</header>`
+);
